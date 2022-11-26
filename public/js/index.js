24d17d3c2d6d99db9f850e92446c0b2724e6c846
Mariano Lopez <lopezmariano3108@gmail.com>
2022-11-26 00:45:42 -0300

    termine el desafio de websockets

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,46 @@ function addMessage() {
     return false
 }
 
+function handleChat() {
+    const mail = document.getElementById("mail").value
+    const contenidoMensaje = document.getElementById("mensaje").value
+    const mensaje = { mensaje: contenidoMensaje, mail: mail, fecha: new Date().toLocaleString() }
+    socket.emit("newMensaje", mensaje)
+    return false
+}
+
+function render(data) {
+    const html = data.map(i => {
+        return (
+            `<p>Nombre: ${i.nombre}, Precio: ${i.precio}, Imagen: <img width="120" src=${i.imagen}></p>`
+        )
+    })
+    document.getElementById("productos").innerHTML = html
+}
+
+
+function renderChat(data) {
+    const html = data.map(i => {
+        return (
+            `<p><span class="fst-italic text-primary">Mail: ${i.mail}</span>, <span class="text-warning">Fecha: ${i.fecha}</span>, <span style="word-wrap: break-word;" class="text-success fst-italic">Mensaje: ${i.mensaje}</span></p>`
+        )
+    })
+    document.getElementById("mensaje").value = ""
+    document.getElementById("chat").innerHTML = html
+}
+
+socket.on("arrayMensajes", data => {
+    renderChat(data)
+})
+
 socket.on("arrayProductos", (data) => {
     render(data)
 })
 
 socket.on("productos", (data) => {
     render(data)
-    console.log("aca tenes la data al iniciar la pag");
 })
 
-function render(data) {
-    const html = data.map(i => {
-        return (
-            `<p>${i.nombre} ${i.precio} <img width="50" src=${i.imagen}></p>`
-        )
-    })
-    document.getElementById("productos").innerHTML = html
-}
-
+socket.on("mensajes", (data) => {
+    renderChat(data)
+})
--- server.js
+++ server.js
@@ -11,13 +11,19 @@ app.use(express.static('./public'))
 const PORT = 8080
 
 const productos = []
+const mensajes = []
 
 io.on("connection", socket => {
     console.log("new cliente conectado")
     socket.emit("productos", productos)
+    socket.emit("mensajes", mensajes)
     socket.on("newProducto", data => {
         productos.push(data)
         io.sockets.emit("arrayProductos", productos)
+    })
+    socket.on("newMensaje", data => {
+        mensajes.push(data)
+        io.sockets.emit("arrayMensajes", mensajes)
     })
 })
 
